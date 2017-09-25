f609f1668cfddee1695d8c0ae75af383ef360fbc
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-09-25 16:35:34 +1000

    add pixi, load image

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //Create the renderer
-var renderer = PIXI.autoDetectRenderer(256, 256);
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view);
@@ -7,5 +7,22 @@ document.body.appendChild(renderer.view);
 //Create a container object called the `stage`
 var stage = new PIXI.Container();
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage);
+//Use Pixi's built-in `loader` object to load an image
+PIXI.loader
+  .add("assets/bg--main-bertrand.jpg")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+    
+  //Create the `cat` sprite from the texture
+  var kaleidoscope = new PIXI.Sprite(
+      PIXI.loader.resources["assets/bg--main-bertrand.jpg"].texture
+  );
+
+  //Add the cat to the stage
+  stage.addChild(kaleidoscope);
+
+  //Render the stage   
+  renderer.render(stage);
+}
