33856d8cacf0a92b2384419b012603425c1b5c9f
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-09-26 17:21:57 +1000

    add translation and rotation

--- src/kaleidoscope.js
+++ src/kaleidoscope.js
@@ -24,6 +24,7 @@ var Kaleidoscope = function (pixiApp) {
         offsetScale: 0.8,
         offsetX: 0.0,
         offsetY: 0.0,
+        radius: 500, // TO BE CHANGED
         // radius: Math.round(Math.sqrt( (self.viewportWidth* self.viewportWidth) + (self.viewportHeight * self.viewportHeight) ) / 2),
         slices: 8,
         zoom: 1.0
@@ -50,24 +51,24 @@ var Kaleidoscope = function (pixiApp) {
             slice.lineStyle(2, 0xffffff);
             slice.position = {x: window.innerWidth/2, y: window.innerHeight/2};
             slice.moveTo(0, 0);
-            slice.arc(0, 0, 500, i*step, i*step + step); // cx, cy, radius, startAngle, endAngle\
+            slice.arc(0, 0, self.variables.radius, i*step, i*step + step); // cx, cy, radius, startAngle, endAngle\
 
             slice.endFill();
             self.sliceContainer.addChild(slice);
 
             // Setup texture
-            var texture = new PIXI.extras.TilingSprite(self.texture, 2000, 2000);
+            var texture = new PIXI.extras.TilingSprite(self.texture, 2048, 2048);
             texture.position = {x: window.innerWidth/2, y: window.innerHeight/2};
             texture.anchor.x = 0.5;
             texture.anchor.y = 0.5;
             texture.mask = slice;
-            texture.rotation = i*step;
+            texture.rotation = Math.PI + i*step;
 
             if (i % 2 !== 0) {
                 texture.anchor.x = 0.5;
                 texture.anchor.y = 0.5;
                 // texture.scale.x *= -1;
-                texture.scale.y *= -1;
+                // texture.scale.y *= -1;
                 // texture.position
             }
 
@@ -77,9 +78,26 @@ var Kaleidoscope = function (pixiApp) {
 
     self.update = function(delta) {
         var sliceImages = self.textureContainer.children;
+        var step = self.TWO_PI / self.variables.slices;
+
         for (var i = 0; i < sliceImages.length; i++) {
+            sliceImages[i].x -= Math.cos(sliceImages[i].rotation);
+            sliceImages[i].y -= Math.sin(sliceImages[i].rotation);
             sliceImages[i].rotation += 0.001;
+            // console.log("index: " + i + ", x: " + sliceImages[i].x );
+            // if (sliceImages[i].x < 200) {
+            //     self.textureContainer.removeChildAt(i);
+            //     var texture = new PIXI.extras.TilingSprite(self.texture, 2000, 2000);
+            //     texture.position = {x: window.innerWidth/2, y: window.innerHeight/2};
+            //     texture.anchor.x = 0.5;
+            //     texture.anchor.y = 0.5;
+            //     texture.mask = self.sliceContainer.getChildAt(i);
+            //     texture.rotation = i*step;
+            //     self.textureContainer.addChild(texture);
+            // }
         }
+        // self.context.translate(self.variables.offsetX - cx, self.variables.offsetY);
+        // console.log(sliceImages[0].x);
     };
 }
 
