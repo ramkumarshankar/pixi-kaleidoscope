bea4a0aa72e937d9de5c86ef3d9c948cbbd91a0d
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-12-01 12:50:50 +1100

    create a separate production config file

---
+++ src/config/prd.js
@@ -0,0 +1,15 @@
+// Kaleidoscope settings
+// Used in Dat.gui for control
+
+var config = {
+  bGui: true,
+  speed: 0.5,
+  reverse: false,
+  animate: true,
+  offsetScale: 0.5, // safe values are between 0.20 and 0.80
+  slices: 6,
+  zoom: 1.0,
+  images: JSON.parse(localStorage.getItem('presetImages'))
+}
+
+module.exports = config;
